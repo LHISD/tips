f49ab2340e6cbabe969b5537a09454555c8bb091
Liberty Hill ISD <cstoltz@libertyhill.txed.net>
2026-09-17 13:18:35 -0500

    Delete LHISD Tips - TEST directory

--- LHISD Tips - TEST/config.js
+++
@@ -1,4 +0,0 @@
-window.LHISD_TIP_CONFIG = Object.freeze({
-  environment: 'TEST',
-  endpoint: 'https://script.google.com/macros/s/AKfycbyh6LupRM-VAOMAmiaBgXQ9ZCgGBNTzIKgz5mgfXrlY3UNEVif1NN9BFAlRopUT3VnmXw/exec'
-});
