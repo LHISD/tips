86379641d6c58fa6b0e7ab74c092646a1c3db5bd
Liberty Hill ISD <cstoltz@libertyhill.txed.net>
2026-09-17 13:10:51 -0500

    Add files via upload

---
+++ LHISD Tips - TEST/config.js
@@ -0,0 +1,4 @@
+window.LHISD_TIP_CONFIG = Object.freeze({
+  environment: 'TEST',
+  endpoint: 'https://script.google.com/macros/s/AKfycbyh6LupRM-VAOMAmiaBgXQ9ZCgGBNTzIKgz5mgfXrlY3UNEVif1NN9BFAlRopUT3VnmXw/exec'
+});
